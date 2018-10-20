e4de8fc0eddc50da2de68d854d8263e5d6b4099a
RealWeeks <weeksjasons@gmail.com>
2018-10-20 16:39:51 -0400

    adds and formats away button

--- src/Awaytime.vue
+++ src/Awaytime.vue
@@ -2,11 +2,16 @@
   <div id="away-time">
     <div class="form-wrapper standard-form">
       <b-form @reset="onReset" v-if="show">
-
+        <b-form  class="away-date-inputs" inline>
         <b-form-group
-                      label="Date">
+                      label="Start date">
           <datepicker v-model="form.start" class="datepicker"></datepicker>
         </b-form-group>
+        <b-form-group
+                      label="End date">
+          <datepicker v-model="form.end" class="datepicker"></datepicker>
+        </b-form-group>
+        </b-form>
         <b-form-group
                       label="Your Name">
           <b-form-select
@@ -22,7 +27,9 @@
           <b-button v-else @click="onSubmit" type="submit" variant="info">Submit</b-button>
         </div> -->
       </b-form>
+
     </div>
+    <b-button @click="handleSetAway" variant="outline-info">Set as away</b-button>
   </div>
 </template>
 
@@ -43,6 +50,9 @@ export default {
       /* Trick to reset/clear native browser form validation state */
       this.show = false;
       this.$nextTick(() => { this.show = true })
+    },
+    handleSetAway(){
+
     }
   },
   computed:{
@@ -53,6 +63,7 @@ export default {
       form: {
         person: null,
         start: '',
+        end:''
       },
       people: [
         { text: 'Select One', value: null },
@@ -65,5 +76,18 @@ export default {
 </script>
 
 <style scoped>
-
+#away-time{
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+}
+.away-date-inputs{
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.btn-outline-info{
+  width: 75%;
+  align-self: center;
+  margin-top: 40px;
+}
 </style>
